e22f23152093bd289ebb5695825008a8826d9513
MyManAy <66086069+MyManAy@users.noreply.github.com>
2023-06-30 14:39:11 -0500

    commented out

--- cardDataUpdater/cardsToFirebase.js
+++ cardDataUpdater/cardsToFirebase.js
@@ -1,22 +1,28 @@
-import fs from "fs";
-import { collection } from "firebase/firestore";
+// import fs from "fs";
+// import { firestore } from "./firebase.js";
+// import { collection, addDoc } from "firebase/firestore";
+// import props from "./cardPropsFromImages.json" assert { type: "json" };
 
-console.log(collection);
-const obj = fs
-  .readdirSync(
-    "C:/Users/nithi/Documents/VSCodeProjects/payments/public/Polygonal Animal Drawings"
-  )
-  .map((file) => ({
-    src: `/Polygonal Animal Drawings/${file}`,
-    animalName: file.match(/- ([A-Z ]+)/i)[1].trim(),
-    rarity: file.match(/([A-Z ]+) -/i)[1],
-    variation: file.match(/(\d+) of/)?.[1],
-    totalVariations: file.match(/(\d+)\.png/)?.[1],
-  }));
+// FOR UPDATING THE JSON CONTAINING CARD PROP DATA
+// const obj = fs
+//   .readdirSync(
+//     "C:/Users/nithi/Documents/VSCodeProjects/payments/public/Polygonal Animal Drawings"
+//   )
+//   .map((file) => ({
+//     src: `/Polygonal Animal Drawings/${file}`,
+//     animalName: file.match(/- ([A-Z ]+)/i)[1].trim(),
+//     rarity: file.match(/([A-Z ]+) -/i)[1],
+//     variation: file.match(/(\d+) of/)?.[1],
+//     totalVariations: file.match(/(\d+)\.png/)?.[1],
+//   }));
+
+// const json = JSON.stringify(obj);
+
+// fs.writeFileSync("cardPropsFromImages.json", json, "utf8");
 
 // DO NOT EXECUTE THE BELOW CODE IF "cards" COLLECTION ALREADY HAS THE CARDS
 
-// const cardsCollection = collection(firestore, "cards");
+// const cardsCollection = collection(firestore, "setCards");
 // const add = async () => {
 //   props.forEach((item) => {
 //     addDoc(cardsCollection, item);
@@ -24,7 +30,3 @@ const obj = fs
 // };
 
 // add();
-
-const json = JSON.stringify(obj);
-
-fs.writeFileSync("cardPropsFromImages.json", json, "utf8");
