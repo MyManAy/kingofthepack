d6664bf7334daab5523bfa60a22f8c9cc1414850
MyManAy <66086069+MyManAy@users.noreply.github.com>
2023-06-30 11:07:44 -0500

    uncommented

--- client/app/utils/weightedRandom.ts
+++ client/app/utils/weightedRandom.ts
@@ -1,5 +1,7 @@
 import { QuerySnapshot, collection } from "firebase/firestore";
+import cardProps from "./cardPropsFromImages.json";
 import { firestore } from "./firebase";
+import { Rarity } from "../components/TradingCard/TradingCard";
 
 export default function weightedRandom(spec: number[]) {
   let table = [] as string[];
@@ -24,21 +26,20 @@ export const getAllCards = async () => {
   });
 };
 
-// const cardProps = fire
-// const rarities: Rarity[] = [
-//   "Common",
-//   "Rare",
-//   "Epic",
-//   "Golden",
-//   "King of the Pack",
-// ];
-// const randomRarityChooser = () =>
-//   rarities[Number(weightedRandom([0.6, 0.3, 0.075, 0.02, 0.005])())];
+const rarities: Rarity[] = [
+  "Common",
+  "Rare",
+  "Epic",
+  "Golden",
+  "King of the Pack",
+];
+const randomRarityChooser = () =>
+  rarities[Number(weightedRandom([0.6, 0.3, 0.075, 0.02, 0.005])())];
 
-// export const randomCardPropsChooser = () => {
-//   const chosenRarity = randomRarityChooser();
-//   const cardsOfRarity = cardProps.filter(
-//     (item) => item.rarity === chosenRarity
-//   );
-//   return cardsOfRarity[Math.floor(Math.random() * cardsOfRarity.length)];
-// };
+export const randomCardPropsChooser = () => {
+  const chosenRarity = randomRarityChooser();
+  const cardsOfRarity = cardProps.filter(
+    (item) => item.rarity === chosenRarity
+  );
+  return cardsOfRarity[Math.floor(Math.random() * cardsOfRarity.length)];
+};
