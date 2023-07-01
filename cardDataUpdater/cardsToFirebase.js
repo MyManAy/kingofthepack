ed7925d6cf3e310158be38701be390c3b393b696
MyManAy <66086069+MyManAy@users.noreply.github.com>
2023-07-01 13:35:44 -0500

    did supabase stuff but no RLS so gitignored

--- cardDataUpdater/cardsToFirebase.js
+++ cardDataUpdater/cardsToFirebase.js
@@ -1,32 +1,28 @@
-// import fs from "fs";
+import fs from "fs";
 // import { firestore } from "./firebase.js";
 // import { collection, addDoc } from "firebase/firestore";
 // import props from "./cardPropsFromImages.json" assert { type: "json" };
 
 // FOR UPDATING THE JSON CONTAINING CARD PROP DATA
-// const obj = fs
-//   .readdirSync(
-//     "C:/Users/nithi/Documents/VSCodeProjects/payments/public/Polygonal Animal Drawings"
-//   )
-//   .map((file) => ({
-//     src: `/Polygonal Animal Drawings/${file}`,
-//     animalName: file.match(/- ([A-Z ]+)/i)[1].trim(),
-//     rarity: file.match(/([A-Z ]+) -/i)[1],
-//     variation: file.match(/(\d+) of/)?.[1],
-//     totalVariations: file.match(/(\d+)\.png/)?.[1],
-//   }));
-
-// const json = JSON.stringify(obj);
-
-// fs.writeFileSync("cardPropsFromImages.json", json, "utf8");
+const obj = fs
+  .readdirSync(
+    "C:/Users/nithi/Documents/VSCodeProjects/payments/public/Polygonal Animal Drawings"
+  )
+  .map((file) => ({
+    src: `/Polygonal Animal Drawings/${file}`,
+    animalName: file.match(/- ([A-Z ]+)/i)[1].trim(),
+    rarity: file.match(/([A-Z ]+) -/i)[1],
+    variation: file.match(/(\d+) of/)?.[1],
+    totalVariations: file.match(/(\d+)\.png/)?.[1],
+  }));
+const json = JSON.stringify(obj);
+fs.writeFileSync("cardPropsFromImages.json", json, "utf8");
 
 // DO NOT EXECUTE THE BELOW CODE IF "cards" COLLECTION ALREADY HAS THE CARDS
-
 // const cardsCollection = collection(firestore, "setCards");
 // const add = async () => {
 //   props.forEach((item) => {
 //     addDoc(cardsCollection, item);
 //   });
 // };
-
 // add();
--- client/README.md
+++ client/README.md
@@ -7,32 +7,54 @@ Stripe Checkout is the fastest way to get started with payments. Included are so
 ### Development
 
 1. Build the application
-~~~shell
+
+```shell
 $ npm install
-~~~
+```
 
 2. _Optional_: download and run the [Stripe CLI](https://stripe.com/docs/stripe-cli)
-~~~shell
+
+```shell
 $ stripe listen --forward-to localhost:3000/api/webhooks
-~~~
+```
 
 3. Run the application
-~~~shell
+
+```shell
 $ STRIPE_WEBHOOK_SECRET=$(stripe listen --print-secret) npm run dev
-~~~
+```
 
 4. Go to [localhost:3000](http://localhost:3000)
 
 ### Production
 
 1. Build the application
-~~~shell
+
+```shell
 $ npm install
 
 $ npm build
-~~~
+```
 
 2. Run the application
-~~~shell
+
+```shell
 $ npm start
-~~~
+```
+
+NEW CARD ADDITION FLOW:
+
+1. download all the images and put them in client/public/Polygonal Animal Drawings
+2. name all them according to the convention "RARITY - NAME VARIATION of TOTAL_VARIATIONS.png"
+3. go to cardDataUpdater folder
+4. uncomment all the imports
+5. uncomment this // FOR UPDATING THE JSON CONTAINING CARD PROP DATA
+6. run with node
+7. uncomment this // DO NOT EXECUTE THE BELOW CODE IF "cards" COLLECTION ALREADY HAS THE CARDS to update changes to database
+8. run with node
+
+TO GENERATE SUPABASE TYPES:
+https://supabase.com/docs/guides/api/rest/generating-types
+
+1. cd into client
+2. npx supabase gen types typescript --project-id "vdbkaqjcvygphoahmtgo" --schema public > app/generated/types_db.ts
