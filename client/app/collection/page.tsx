5e70c7e13c6cb269f4e562665937d30b56a8c08a
MyManAy <66086069+MyManAy@users.noreply.github.com>
2023-07-02 09:17:10 -0500

    collection?

--- client/app/collection/page.tsx
+++ client/app/collection/page.tsx
@@ -1,7 +1,75 @@
-import "@/app/components/MagicCard/MagicCard.css";
+"use client";
 
-export interface IAppProps {}
+import { useSearchParams } from "next/navigation";
+import CollectionCard from "@/app/components/CollectionCard/CollectionCard";
+import "./page.css";
+import { useEffect, useState } from "react";
+import { supabase } from "../utils/supabase";
+import { IAppProps } from "@/app/components/CollectionCard/CollectionCard";
 
-export default function App(props: IAppProps) {
-  return <div>hello</div>;
+export default function App() {
+  const searchParams = useSearchParams();
+  const [cardProps, setCardProps] = useState(null as null | IAppProps[]);
+  const setId = searchParams.get("setId");
+  const name = searchParams.get("name");
+
+  const idk = async () => {
+    const { data: cards } = await supabase
+      .from("set")
+      .select(
+        `
+    card (
+      *
+    )
+  `
+      )
+      .eq("id", setId)
+      .single();
+
+    const props = cards!.card;
+    console.log(JSON.stringify(cards, null, 4));
+
+    const { data: userCardIds } = await supabase
+      .from("user")
+      .select(
+        `
+      openedPack (
+        circulationCard (
+          card (
+            id
+          )
+        )
+      )
+   `
+      )
+      .eq("email", "nithin.monni@gmail.com")
+      .single();
+
+    const ids = userCardIds!.openedPack.flatMap((op) =>
+      op.circulationCard.flatMap((cc) => cc.card!.id)
+    );
+    console.log(ids);
+
+    let propWithCount: IAppProps[] = [];
+    for (const prop of props) {
+      const count = ids.filter((id) => id === prop.id).length;
+      propWithCount.push({ ...props, count: count } as any);
+    }
+    setCardProps(propWithCount);
+  };
+
+  useEffect(() => {}, []);
+  return (
+    <>
+      <div className={"text-white font-bold text-4xl mb-10"}>{name}</div>
+      <div className={"flex flex-row flex-wrap justify-center"}>
+        {cardProps &&
+          cardProps.map((item, index) => (
+            <div className={"p-5"}>
+              <CollectionCard {...item} key={index} />
+            </div>
+          ))}
+      </div>
+    </>
+  );
 }
---
+++ client/app/components/CollectionCard/CollectionCard.tsx
@@ -0,0 +1,100 @@
+import "./Iridescent.css";
+import "./Neon.css";
+import "./Badge.css";
+
+export type Rarity = "Common" | "Rare" | "Epic" | "Golden" | "King of the Pack";
+
+type RarityStyleMapping = {
+  [key in Rarity]: {
+    bgStyle: string;
+    nameStyle: string;
+    rarityStyle: string;
+  };
+};
+const rarityStyleMapping: RarityStyleMapping = {
+  Common: {
+    bgStyle: "bg-zinc-300",
+    nameStyle: "text-black opacity-80",
+    rarityStyle: "text-zinc-500 opacity-80",
+  },
+  Rare: {
+    bgStyle: "bg-indigo-600",
+    nameStyle: "text-white opacity-90",
+    rarityStyle: "text-zinc-300 opacity-80",
+  },
+  Epic: {
+    bgStyle: "bg-fuchsia-400",
+    nameStyle: "text-black opacity-80",
+    rarityStyle: "text-zinc-800 opacity-70",
+  },
+  Golden: {
+    bgStyle: "bg-orange-500",
+    nameStyle: "text-white opacity-100",
+    rarityStyle: "text-zinc-100 opacity-70",
+  },
+  "King of the Pack": {
+    bgStyle: "bg-zinc-300",
+    nameStyle: "text-black opacity-80",
+    rarityStyle: "text-[#3f0719] opacity-80",
+  },
+};
+
+const containers = [
+  "h-[45vh] w-[calc(45vh/1.5)] grid grid-cols-[1fr_5fr_1fr] grid-rows-[1fr_4fr_1fr] rounded-md overcursor-pointer opacity-10 -z-10",
+  "h-[45vh] w-[calc(45vh/1.5)] grid grid-cols-[1fr_5fr_1fr] grid-rows-[1fr_4fr_1fr] rounded-md cursor-pointer -z-10",
+];
+
+export interface IAppProps {
+  rarity: string;
+  animalName: string;
+  src: string;
+  variation?: number;
+  totalVariations?: number;
+  count: number;
+}
+
+export default function App({
+  rarity,
+  animalName,
+  src,
+  variation = 1,
+  totalVariations = 1,
+  count,
+}: IAppProps) {
+  return (
+    <div
+      className={`${containers[+(count > 0)]} ${
+        rarity === "King of the Pack"
+          ? "iridescent"
+          : rarityStyleMapping[rarity as Rarity].bgStyle
+      }`}
+    >
+      <div className="w-6 h-7 rounded-tl-md rounded-br-md bg-black text-white flex font-black justify-center items-center">
+        {count}
+      </div>
+      <div
+        className={`font-semibold flex justify-center items-center text-center whitespace-nowrap text-clip ${
+          rarityStyleMapping[rarity as Rarity].nameStyle
+        } ${rarity === "King of the Pack" ? "text-lg" : "text-xl"}`}
+      >
+        {animalName}
+      </div>
+      <div></div>
+      <div></div>
+      <div className="rounded justify-center items-center">
+        <img className="rounded" alt={animalName} src={src} />
+      </div>
+      <div></div>
+      <div></div>
+      <div
+        className={`font-medium text-sm flex justify-center items-center ${
+          rarityStyleMapping[rarity as Rarity].rarityStyle
+        }`}
+      >
+        {rarity === "King of the Pack"
+          ? rarity
+          : `${rarity} ${variation}/${totalVariations}`}
+      </div>
+    </div>
+  );
+}
