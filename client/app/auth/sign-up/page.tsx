34336b5258be693ca1f1d4f48d0d686bc4546629
MyManAy <66086069+MyManAy@users.noreply.github.com>
2023-07-02 20:48:47 -0500

    sign up auth ui change

--- client/app/auth/sign-up/page.tsx
+++ client/app/auth/sign-up/page.tsx
@@ -2,8 +2,8 @@
 
 import { ChangeEvent, FormEvent, useState } from "react";
 import { supabase } from "../../utils/supabase";
-// import "./page.css";
 import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 export default function App() {
   const [email, setEmail] = useState("");
@@ -34,7 +34,11 @@ export default function App() {
   };
 
   return (
-    <Auth supabaseClient={supabase} />
+    <Auth
+      supabaseClient={supabase}
+      appearance={{ theme: ThemeSupa }}
+      theme="dark"
+    />
     // <main className="main">
     //   <div className="container">
     //     <section className="wrapper">
