635a0cc2b68262272faf3dc91aa52d9a81b0b2aa
MyManAy <66086069+MyManAy@users.noreply.github.com>
2023-07-03 11:53:22 -0500

    nav

--- client/app/components/ProtectedLayout.tsx
+++ client/app/components/ProtectedLayout.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { supabase } from "../utils/supabase";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function ProtectedLayout({
   children,
