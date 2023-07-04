a0fe366f0c0fc89ebd56866848e39adf1d008661
MyManAy <66086069+MyManAy@users.noreply.github.com>
2023-07-03 21:54:34 -0500

    yoooo text works

--- client/app/api/supabase/route.ts
+++ client/app/api/supabase/route.ts
@@ -1,8 +1,12 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request, res: any) {
-  const body = await req.text();
-  console.log(body);
+  const text = await req.text();
+  const json = JSON.parse(text) as any;
+  const record = json.record;
+  const { email, user_meta_data, id } = record;
+  const username = user_meta_data.name ?? user_meta_data.username;
+  console.log(email, username, id);
 
   return NextResponse.json({
     recieved: true,
