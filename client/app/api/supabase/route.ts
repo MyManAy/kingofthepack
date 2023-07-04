2f027663d02b673053e9bb3f88a23b354f176394
MyManAy <66086069+MyManAy@users.noreply.github.com>
2023-07-03 21:45:08 -0500

    text?

--- client/app/api/supabase/route.ts
+++ client/app/api/supabase/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request, res: any) {
-  const body = req;
+  const body = req.text();
   console.log(body);
 
   return NextResponse.json({
