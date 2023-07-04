c009c5caaead21c02fc1ac5edc32225b9cc7da8b
MyManAy <66086069+MyManAy@users.noreply.github.com>
2023-07-04 14:29:08 -0500

    static main page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,27 +1,11 @@
-"use client";
-
-import React from "react";
-import { loadStripe } from "@stripe/stripe-js";
+// import { loadStripe } from "@stripe/stripe-js";
 import MagicCard from "./components/MagicCard/MagicCard";
 import ProtectedLayout from "./components/ProtectedLayout";
 import GreenButton from "./components/GreenButton";
 
-loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+// loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
 export default () => {
-  React.useEffect(() => {
-    const query = new URLSearchParams(window.location.search);
-    if (query.get("success")) {
-      console.log("Order placed! You will receive an email confirmation.");
-    }
-
-    if (query.get("canceled")) {
-      console.log(
-        "Order canceled -- continue to shop around and checkout when you’re ready."
-      );
-    }
-  }, []);
-
   return (
     <ProtectedLayout>
       <div className="flex flex-col gap-10 justify-center align-middle">
