8b21f907bdb8ed5a25db3f7aa398c2353a516b9a
MyManAy <66086069+MyManAy@users.noreply.github.com>
2023-07-03 21:57:48 -0500

    raw_user_meta_data

--- client/app/api/supabase/route.ts
+++ client/app/api/supabase/route.ts
@@ -4,8 +4,8 @@ export async function POST(req: Request, res: any) {
   const text = await req.text();
   const json = JSON.parse(text) as any;
   const record = json.record;
-  const { email, user_meta_data, id } = record;
-  const username = user_meta_data.name ?? user_meta_data.username;
+  const { email, raw_user_meta_data, id } = record;
+  const username = raw_user_meta_data.name ?? raw_user_meta_data.username;
   console.log(email, username, id);
 
   return NextResponse.json({
