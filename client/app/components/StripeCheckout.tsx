9af80e814af7823a8b9884488bd64d5cb2eaa25a
MyManAy <66086069+MyManAy@users.noreply.github.com>
2023-07-04 17:42:18 -0500

    see if stripe checkout works without client now

--- client/app/components/StripeCheckout.tsx
+++ client/app/components/StripeCheckout.tsx
@@ -16,33 +16,6 @@ export default function App(props: IAppProps) {
           Checkout
         </button>
       </section>
-      <style jsx>
-        {`
-          section {
-            background: #ffffff;
-            display: flex;
-            flex-direction: column;
-            width: 100%;
-            border-radius: 6px;
-            justify-content: space-between;
-          }
-          button {
-            z-index: 3;
-            height: 36px;
-            background: #55d662;
-            border-radius: 4px;
-            color: white;
-            border: 0;
-            font-weight: 600;
-            cursor: pointer;
-            transition: all 0.2s ease;
-            box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
-          }
-          button:hover {
-            opacity: 0.8;
-          }
-        `}
-      </style>
     </form>
   );
 }
