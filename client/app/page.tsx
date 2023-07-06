496437bbba09646440312d0beabddf2793ee6049
MyManAy <66086069+MyManAy@users.noreply.github.com>
2023-07-06 00:07:04 -0500

    again

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,6 @@
-// import { loadStripe } from "@stripe/stripe-js";
 import MagicCard from "./components/MagicCard/MagicCard";
 import ProtectedLayout from "./components/ProtectedLayout";
 import GreenButton from "./components/GreenButton";
-
-// loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
 export default () => {
   return (
@@ -12,7 +9,6 @@ export default () => {
         <div className="h-[65vh] w-[calc(65vh/1.5)] text-4xl">
           <MagicCard />
         </div>
-        {/* <StripeCheckout /> */}
         <div className="flex flex-col gap-5 justify-center align-middle">
           <GreenButton text="Packs" route="/packs" />
           <GreenButton text="Collections" route="/collections" />
