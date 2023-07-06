47fd21487ada9f078bb2601894221b830b3e7225
MyManAy <66086069+MyManAy@users.noreply.github.com>
2023-07-06 11:41:06 -0500

    api route

---
+++ client/app/api/get_collection_props/route.ts
@@ -0,0 +1,69 @@
+import { IAppProps } from "@/app/components/CollectionCard/CollectionCard";
+import { supabase } from "@/app/utils/supabase";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request, res: Response) {
+  const { searchParams } = new URL(req.url);
+  const setId = searchParams.get("setId");
+  const email = "nithinmonni@gmail.com";
+
+  const { data: set } = await supabase
+    .from("set")
+    .select(
+      `
+        name,
+        weighting (
+          rarity,
+          weighting
+        ),
+        card (
+          *
+        )
+  `
+    )
+    .eq("id", setId!)
+    .single();
+
+  const { card, weighting, name } = set!;
+  const totalCards = card.length;
+  const setName = name;
+
+  const { data: userCardIds } = await supabase
+    .from("user")
+    .select(
+      `
+      openedPack (
+        circulationCard (
+          card (
+            id
+          )
+        )
+      )
+   `
+    )
+    .eq("email", email)
+    .single();
+
+  const ids = userCardIds!.openedPack.flatMap((op) =>
+    op.circulationCard.flatMap((cc) => cc.card!.id)
+  );
+  console.log(ids);
+
+  let cardsCollected = 0;
+  let propWithCount: IAppProps[] = [];
+  for (const prop of card) {
+    const count = ids.filter((id) => id === prop.id).length;
+    if (count > 0) cardsCollected++;
+    propWithCount.push({ ...prop, count: count });
+  }
+  const getWeightingFromRarity = (rarity: string) =>
+    weighting.find((item) => item.rarity === rarity);
+  const sorted = propWithCount.sort(
+    (a, b) =>
+      getWeightingFromRarity(b.rarity)!.weighting -
+      getWeightingFromRarity(a.rarity)!.weighting
+  );
+  const cardProps = sorted;
+
+  return NextResponse.json({ setName, totalCards, cardsCollected, cardProps });
+}
--- client/app/components/Collection/CollectionPage.tsx
+++ client/app/components/Collection/CollectionPage.tsx
@@ -1,71 +1,16 @@
 import CollectionCard from "@/app/components/CollectionCard/CollectionCard";
 import "./page.css";
-import { supabase } from "@/app/utils/supabase";
 import { IAppProps } from "@/app/components/CollectionCard/CollectionCard";
 
 export default async function App({ setId }: { setId: string }) {
   // const { data } = await supabase.auth.getUser();
   // const userEmail = data.user?.email!;
   // const email = emailMinify(userEmail);
-  const email = "nithinmonni@gmail.com";
-
-  const { data: set } = await supabase
-    .from("set")
-    .select(
-      `
-        name,
-        weighting (
-          rarity,
-          weighting
-        ),
-        card (
-          *
-        )
-  `
-    )
-    .eq("id", setId!)
-    .single();
-
-  const { card, weighting, name } = set!;
-  const totalCards = card.length;
-  const setName = name;
-
-  const { data: userCardIds } = await supabase
-    .from("user")
-    .select(
-      `
-      openedPack (
-        circulationCard (
-          card (
-            id
-          )
-        )
-      )
-   `
-    )
-    .eq("email", email)
-    .single();
-
-  const ids = userCardIds!.openedPack.flatMap((op) =>
-    op.circulationCard.flatMap((cc) => cc.card!.id)
-  );
-  console.log(ids);
-
-  let cardsCollected = 0;
-  let propWithCount: IAppProps[] = [];
-  for (const prop of card) {
-    const count = ids.filter((id) => id === prop.id).length;
-    if (count > 0) cardsCollected++;
-    propWithCount.push({ ...prop, count: count });
-  }
-  const getWeightingFromRarity = (rarity: string) =>
-    weighting.find((item) => item.rarity === rarity);
-  const sorted = propWithCount.sort(
-    (a, b) =>
-      getWeightingFromRarity(b.rarity)!.weighting -
-      getWeightingFromRarity(a.rarity)!.weighting
-  );
-  const cardProps = sorted;
+  const res = await fetch(`/api/get_collection_props?setId=${setId}`, {
+    cache: "no-store",
+  });
+  const json = await res.json();
+  const { setName, totalCards, cardsCollected, cardProps } = json;
 
   return (
     <>
@@ -92,7 +37,7 @@ export default async function App({ setId }: { setId: string }) {
         </div>
       </div>
       <div className={"flex flex-row flex-wrap justify-center"}>
-        {cardProps.map((item, index) => (
+        {cardProps.map((item: IAppProps, index: number) => (
           <div className={"p-5"}>
             <CollectionCard {...item} key={index} />
           </div>
