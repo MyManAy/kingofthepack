7ca9cb4c370e2d0edcbb8c7edb9b3e4aa8379a17
MyManAy <66086069+MyManAy@users.noreply.github.com>
2024-01-12 12:52:24 -0600

    hopefully this adds user to google oauth too

--- client/app/api/supabase/route.ts
+++ client/app/api/supabase/route.ts
@@ -5,13 +5,30 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request, res: any) {
   const text = await req.text();
   const json = JSON.parse(text) as any;
-  const record = json.record;
-  const { email, raw_user_meta_data, id, email_confirmed_at } = record;
+
+  type Record = {
+    email: string;
+    raw_user_meta_data: {
+      name?: string;
+      username?: string;
+    };
+    id: string;
+    email_confirmed_at: string;
+    provider: string;
+  };
+  const record: Record = json.record;
+  const {
+    email,
+    raw_user_meta_data,
+    id,
+    email_confirmed_at,
+    provider,
+  } = record;
   const username = raw_user_meta_data.name ?? raw_user_meta_data.username;
-  if (email_confirmed_at) {
+  if (email_confirmed_at || provider.toLowerCase() === "google") {
     await supabase
       .from("user")
-      .insert({ email: emailMinify(email), username: username, id })
+      .insert({ email: emailMinify(email), username: username!, id })
       .select();
   }
 
